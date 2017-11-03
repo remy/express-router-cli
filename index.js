cf0ae3419cc3e2cdf0f3466592228c06e51b3473
Remy Sharp <remy@remysharp.com>
2017-11-03 12:47:20 +0000

    feat: track delta in routes
    Also fix deeply cached modules - and wipe them out as appropriate.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const detect = require('detect-port');
 const chokidar = require('chokidar');
 const chalk = require('chalk');
-const decache = require('decache');
 const clean = require('clean-stacktrace');
 const { promisify } = require('util');
 const { resolve, dirname } = require('path');
@@ -12,21 +11,28 @@ const stat = promisify(fs.stat);
 const getRoutes = (stack, root = '') => {
   return stack.reduce((acc, curr) => {
     if (curr.route && curr.route.path) {
-      if (Array.isArray(curr.route.path)) {
-        acc.push(...curr.route.path.map(path => root + path));
-      } else acc.push(root + curr.route.path);
+      const path = Array.isArray(curr.route.path)
+        ? curr.route.path
+        : [curr.route.path];
+
+      const method = Object.keys(curr.route.methods)
+        .map(_ => _.toUpperCase())
+        .shift();
+      acc.push(...path.map(path => `${method.padEnd(4, ' ')} ${root}${path}`));
+
       return acc;
     }
 
     if (curr.handle.stack) {
       acc.push(
-        getRoutes(
+        ...getRoutes(
           curr.handle.stack,
-          curr.regexp
-            .toString()
-            .replace(/^\/\^/, '')
-            .replace(/\\\/\?\(\?=\\\/\|\$\)\/i/, '')
-            .replace(/\\\//g, '/')
+          root +
+            curr.regexp
+              .toString()
+              .replace(/^\/\^/, '')
+              .replace(/\\\/\?\(\?=\\\/\|\$\)\/i/, '')
+              .replace(/\\\//g, '/')
         )
       );
     }
@@ -35,6 +41,29 @@ const getRoutes = (stack, root = '') => {
   }, []);
 };
 
+const diff = (old, changed) => {
+  const res = {
+    add: [],
+    remove: [],
+  };
+
+  changed.forEach(item => {
+    if (!old.includes(item)) {
+      res.add.push(item);
+    }
+  });
+
+  old.forEach(item => {
+    if (!changed.includes(item)) {
+      res.remove.push(item);
+    }
+  });
+
+  return res;
+};
+
+let lastRouteLength = [];
+
 const main = async (target, _port, first = false) => {
   const valid = await stat(target);
   if (!valid) {
@@ -42,9 +71,16 @@ const main = async (target, _port, first = false) => {
   }
 
   const mod = resolve(process.cwd(), target);
-  decache(mod);
-
   const dir = valid.isDirectory() ? target : dirname(target);
+  const root = resolve(dir);
+
+  // clear all required modules matching the dir
+  Object.keys(require.cache).forEach(path => {
+    if (path.startsWith(root)) {
+      delete require.cache[path];
+    }
+  });
+
   const app = require('express')();
   const port = await detect(_port);
   const server = app.listen(port);
@@ -53,15 +89,35 @@ const main = async (target, _port, first = false) => {
 
     app.use(router);
 
-    console.log(chalk.gray(`\n> Mounted on http://localhost:${port}`));
     if (first)
+      console.log(chalk.gray(`\n> Mounted on http://localhost:${port}`));
+
+    const routes = getRoutes(router.stack, `http://localhost:${port}`);
+
+    console.log();
+
+    if (first) {
+      // just dump on screen
       console.log(
-        chalk.gray(
-          getRoutes(router.stack)
-            .map(path => `- http://localhost:${port}${path}`)
-            .join('\n')
-        ) + '\n'
+        chalk.gray(routes.map(path => `* ${path}`).join('\n')) + '\n'
       );
+    } else {
+      const delta = diff(lastRouteLength, routes);
+      console.log(
+        routes
+          .map(path => {
+            if (delta.add.includes(path)) {
+              return chalk.bgGreen.white(`+ ${path}`);
+            }
+
+            return chalk.grey(`* ${path}`);
+          })
+          .concat(delta.remove.map(path => chalk.red(`- ${path}`)))
+          .join('\n') + '\n'
+      );
+    }
+
+    lastRouteLength = routes;
   } catch (error) {
     console.log(chalk.red(`> Failed to mount "${target}", waiting for change`));
     console.log(error);
